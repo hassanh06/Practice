789ac4cb47aedeb5363d3c6164b3391194ad0645
Hassanh06 <hassan_arib@hotmail.com>
2020-07-19 03:17:06 -0400

    second commit

--- DoSwitch.java
+++ DoSwitch.java
@@ -2,7 +2,7 @@ package practice;
 
 import java.util.Scanner;
 
-public class DoSwitchEnm {
+public class DoSwitch {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
